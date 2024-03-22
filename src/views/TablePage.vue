d01e0fe13ff382fe4b6c07ef347acb79f418ac9e
Vlad Crihan <vladc99@gmail.com>
2024-03-22 19:57:17 -0400

    Added the new components

---
+++ src/views/TablePage.vue
@@ -0,0 +1,14 @@
+<template>
+    <Table />
+</template>
+
+<script>
+import Table from "../components/Table.vue";
+
+export default {
+    name: "Tables-general",
+    components: {
+        Table
+    }
+}
+</script>
